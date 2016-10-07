9822702dd737df0d12d1e57575fb15cae02f9343
sabatinipeter <sabatinipeter@gmail.com>
2016-10-07 15:10:56 -0400

    Testing port change

--- web.js
+++ web.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
      res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(process.env.PORT || 5000, function() {
+  console.log('Example app listening on port 5000!');
 });
