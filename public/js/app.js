75006c8d2529be18555fb38194f31f05751e570c
sabatinipeter <sabatinipeter@gmail.com>
2016-10-07 14:56:14 -0400

    First Commit

---
+++ public/js/app.js
@@ -0,0 +1,317 @@
+var teaming = {};
+
+teaming.teams = [
+    {
+        "name":"Route One Desking"
+    },
+    {
+        "name":"Route One Menu"
+    },
+    {
+        "name":"ACSI"
+    },
+    {
+        "name":"JIS"
+    },
+    {
+        "name":"Ford - GForce"
+    },
+    {
+        "name":"Ford - FMCC"
+    },
+    {
+        "name":"Ford - Agile COE"
+    },
+    {
+        "name":"Ford - Falcon"
+    },
+    {
+        "name":"Ford - PDO"
+    },
+];
+
+teaming.people = [
+        {
+            "name": "Matt Staffeld",
+            "image": "images/matthewstaffeld.png",
+            "position": "Software Journeyman",
+            "notes":  "Javascript"
+        },
+        {
+            "name": "Bree Lane",
+            "image": "images/breelane.jpg",
+            "position": "Delivery Lead",
+            "notes": "Git"
+        },
+        {
+            "name": "Adam Gartee",
+            "image": "images/adamgartee.png",
+            "position": "Software Journeyman",
+            "notes": ".net"
+        },
+        {
+            "name": "Aladin Gohar",
+            "image": "images/aladingohar.jpg",
+            "position": "Delivery Manager"
+        },
+        {
+            "name": "Amitai Schleier",
+            "image": "images/amitaischleier.jpg",
+            "position": "Software Craftsman"
+        },
+        {
+            "name": "Angela Peat",
+            "image": "images/angelapeat.png",
+            "position": "Delivery Lead"
+        },
+        {
+            "name": "Aziz Harawala",
+            "image": "images/azizhararwala.jpg",
+            "position": "Software Craftsman"
+        },
+        {
+            "name": "Ben Tidman",
+            "image": "images/bentidman.jpg",
+            "position": "Software Journeyman",
+            "notes":  "Javascript"
+        },
+        {
+            "name": "Bill Koester",
+            "image": "images/billkoester.jpg",
+            "position": "Software Journeyman"
+        },
+        {
+            "name": "Brett Lilley",
+            "image": "images/brettlilley.jpg",
+            "position": "Software Journeyman",
+            "notes": ".net"
+        },
+        {
+            "name": "Chris Atkins",
+            "image": "images/chrisatkins.jpg",
+            "position": "Software Craftsman"
+        },
+        {
+            "name": "Christopher Bibbs",
+            "image": "images/Christopherbibbs.png",
+            "position": "Delivery Lead"
+        },
+        {
+            "name": "Clay Dowling",
+            "image": "images/claydowling.jpg",
+            "position": "Software Journeyman"
+        },
+        {
+            "name": "Dan Davis",
+            "image": "images/dandavis.png",
+            "position": "Delivery Lead"
+        },
+        {
+            "name": "Dan Putman",
+            "image": "images/danputman.png",
+            "position": "Software Craftsman",
+            "notes": "beard"
+        },
+        {
+            "name": "David Zuidema",
+            "image": "images/davidzuidema.jpeg",
+            "position": "Software Journeyman"
+        },
+        {
+            "name": "Edwin Aoraha",
+            "image": "images/edwinaoraha.png",
+            "position": "Software Apprentice"
+        },
+        {
+            "name": "Erik Przekop",
+            "image": "images/erikprzekop.jpg",
+            "position": "Software Craftsman"
+        },
+        {
+            "name": "Fred Estabrook",
+            "image": "images/fredestabrook.png",
+            "position": "Delivery Lead"
+        },
+        {
+            "name": "Gabe Ilko",
+            "image": "images/gabeilko.jpg",
+            "position": "Software Craftsman"
+        },
+        {
+            "name": "Jeeva Adarahah",
+            "image": "images/jeevanadarahah.jpg",
+            "position": "Software Craftsman"
+        },
+        {
+            "name": "Jeff Hoover",
+            "image": "images/jeffhoover.png",
+            "position": "Software Journeyman"
+        },
+        {
+            "name": "Jennifer Folkerth",
+            "image": "images/jenniferfolkerth.jpg",
+            "position": "Software Journeyman"
+        },
+        {
+            "name": "Joe Dluzen",
+            "image": "images/joedluzen.jpg",
+            "position": "Software Journeyman"
+        },
+        {
+            "name": "John Gartee",
+            "image": "images/johngartee.png",
+            "position": "Software Craftsman",
+            "notes": "beard"
+        },
+        {
+            "name": "Jonathan Baugh",
+            "image": "images/jonathanbaugh.jpg",
+            "position": "Delivery Lead/Experience Architect",
+            "notes": "beard"
+        },
+        {
+            "name": "Josh Bechard",
+            "image": "images/joshbechard.jpg",
+            "position": "Software Journeyman"
+        },
+        {
+            "name": "Josh Ribolla",
+            "image": "images/joshribolla.jpg",
+            "position": "Experience Architect"
+        },
+        {
+            "name": "Mark Henke",
+            "image": "images/markhenke photo.jpg",
+            "position": "Software Journeyman",
+            "notes": "beard"
+        },
+        {
+            "name": "Mark Protas",
+            "image": "images/markprotas.jpg",
+            "position": "Software Craftsman",
+            "notes": "Java beard"
+
+        },
+        {
+            "name": "Meera Srinivasan",
+            "image": "images/meerasrinivasan.png",
+            "position": "Delivery Lead"
+        },
+        {
+            "name": "Michele Cresmen-Block",
+            "image": "images/michelecresmenblock.png",
+            "position": "Delivery Manager"
+        },
+        {
+            "name": "Paul Glinski",
+            "image": "images/paulglinski.jpg",
+            "position": "Software Journeyman"
+        },
+        {
+            "name": "Peter Sabatini",
+            "image": "images/petersabatini.png",
+            "position": "Software Journeyman",
+            "notes": "Javascript"
+
+        },
+        {
+            "name": "Ralph Hale",
+            "image": "images/ralphhale.jpg",
+            "position": "Software Craftsman"
+        },
+        {
+            "name": "Rob Murdock",
+            "image": "images/robmurdock.png",
+            "position": "Software Craftsman",
+            "notes": "beard"
+        },
+        {
+            "name": "Stephen Cavaliere",
+            "image": "images/stephencavaliere.jpg",
+            "position": "Software Craftsman"
+        },
+        {
+            "name": "Theo Chupp",
+            "image": "images/theochupp.jpg",
+            "position": "Software Journeyman",
+            "notes": "beard"
+        },
+        {
+            "name": "Todd Hurley",
+            "image": "images/toddhurley.jpg",
+            "position": "Software Craftsman"
+        },
+        {
+            "name": "Tom Puricelli",
+            "image": "images/tompuricelli pic.jpg",
+            "position": "Delivery Lead"
+        },
+        {
+            "name": "Will Gibbins",
+            "image": "images/willgibbins.jpg",
+            "position": "Delivery Lead",
+            "notes": "beard"
+        }
+    ];
+
+teaming.newPerson = {
+    "name": "New Pillarite",
+    "image": "images/new.jpg",
+    "position": "Software Journeyman"
+};
+
+teaming.addNewPerson = function() {
+    $('#peopleDiv').append(teaming.renderPersonTemplate(teaming.newPerson, 'New'));
+};
+
+teaming.addNewTeam = function() {
+    $('#teamDiv').append(teaming.renderTeamTemplate({"name":"New Pillar Team"}, 'New'));
+}
+
+teaming.renderPeople = function () {
+    teaming.renderItems('#peopleDiv', teaming.people, teaming.renderPersonTemplate);
+};
+
+teaming.renderTeams = function() {
+    teaming.renderItems('#teamDiv', teaming.teams, teaming.renderTeamTemplate);
+};
+
+teaming.renderItems = function(targetDiv, collection, renderFunction) {
+    var html = "";
+    var id = 1;
+
+    _.each(collection, function(element) {
+        html += renderFunction(element, id);
+        id++;
+    });
+
+    $(targetDiv).html(html);
+}
+
+teaming.renderPersonTemplate = function(person, id) {
+    var template = $('#personTemplate').html();
+    Mustache.parse(template);
+
+    return Mustache.render(template, {id: "drag" + id, img: person.image, name: person.name, position: person.position, notes: person.notes});
+};
+
+teaming.renderTeamTemplate = function(team, id) {
+    var template = $('#teamTemplate').html();
+    Mustache.parse(template);
+
+    return Mustache.render(template, {id: "team" + id, teamName: team.name});
+};
+
+$(function() {
+    teaming.renderTeams();
+    teaming.renderPeople();
+    $('#addPersonButton').click(function() {
+        teaming.addNewPerson();
+        $('#closeAddPersonModal').trigger('click');
+    });
+
+    $('#addTeamButton').click(function() {
+        teaming.addNewTeam();
+        $('#closeAddTeamModal').trigger('click');
+    })
+});
