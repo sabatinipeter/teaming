20aee1a3c261632819ea1558ada95dd72291ec77
Gregory Haskins <greg@greghaskins.com>
2016-11-08 22:59:31 -0500

    Start with just a sample team

--- public/js/app.js
+++ public/js/app.js
@@ -2,41 +2,8 @@ var teaming = {};
 
 teaming.teams = [
     {
-        "name":"Route One Desking",
-        "roles":["DL", "Dev", "Dev"]
-    },
-    {
-        "name":"Route One Menu",
-        "roles":["DL", "DL", "Dev", "Dev", "Dev", "Dev", "Apr"]
-    },
-    {
-        "name":"ACSI",
-        "roles":["DL", "TL", "Dev", "Dev", "Dev", "Dev", "Dev","Dev", "XD", "XD"]
-    },
-    {
-        "name":"JIS",
-        "roles":["DM", "DL", "DL", "TL", "Dev", "Dev", "Dev", "Dev", "Dev", "Dev", "Dev", "XD"]
-    },
-    {
-        "name":"Ford - FMCC",
-        "roles":["DM", "DL", "Dev", "Dev", "Dev", "1/2 Dev"]
-    },
-    {
-        "name":"Ford - GForce",
-        "roles":["DM", "DL", "TL", "BA", "Dev", "Dev", "Dev", "1/2 Dev", "1/2 Dev"]
-    },
-    {
-        "name":"Ford - Agile COE",
-        "roles":["Other", "Other", "Other"]
-
-    },
-    {
-        "name":"Ford - Falcon",
-        "roles":["DL", "Dev", "Dev", "Dev"]
-    },
-    {
-        "name":"Ford - PDO",
-        "roles":["Other", "Other"]
+        "name":"Sample Team",
+        "roles":["DL", "TL", "Dev", "Dev", "Dev", "XA", "XD"]
     }
     ];
 
