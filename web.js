a09ccc17a22a8927677b0062bb6d9a4c5c8f4b67
sabatinipeter <sabatinipeter@gmail.com>
2017-02-18 21:31:34 -0500

    V2

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var path    = require("path");
 app.use('/', express.static(__dirname + '/public/'))
 
 app.get('/', function (req, res) {
-     res.sendFile(path.join(__dirname+'/index.html'));
+     res.sendFile(path.join(__dirname+'/dashboard.html'));
 });
 
 app.listen(process.env.PORT || 5000, function() {
